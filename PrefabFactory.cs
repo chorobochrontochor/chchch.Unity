8cbc360d9ac8ac8bd9091a3d90f08ff9a75f8995
chorobochrontochor <marek.jelinek@live.com>
2017-10-01 12:08:35 +0200

    added prefab factory

---
+++ PrefabFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chchch
+{
+    public static class PrefabFactory
+    {
+        public static GameObject Create(GameObject p_prefab, Vector3 p_position, Quaternion p_rotation, Transform p_parent = null)
+        {
+            GameObject instance = GameObject.Instantiate<GameObject>(p_prefab, p_position, p_rotation);
+
+            if (p_parent != null)
+            {
+                instance.transform.SetParent(p_parent, false);
+            }
+
+            return instance;
+        }
+
+        public static GameObject Create(GameObject p_prefab, Vector3 p_position, Quaternion p_rotation, GameObject p_parent = null)
+        {
+            return Create(p_prefab, p_position, p_rotation, p_parent.transform);
+        }
+
+        public static GameObject Create(GameObject p_prefab, Vector3 p_position, Quaternion p_rotation, Component p_parent = null)
+        {
+            return Create(p_prefab, p_position, p_rotation, p_parent.transform);
+        }
+    }
+}
