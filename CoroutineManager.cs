08d9fa8a35b8049cb30f65a6559a51571ac7f492
chorobochrontochor <marek.jelinek@live.com>
2017-10-01 11:48:55 +0200

    added coroutine manager

---
+++ CoroutineManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chchch
+{
+    public static class CoroutineManager
+    {
+        private class _coroutineBehaviour : MonoBehaviour { };
+        private static _coroutineBehaviour _monoBehaviour;
+
+        public static Coroutine Start(IEnumerator p_enumerator)
+        {
+            if (_monoBehaviour == null)
+            {
+                GameObject gameObject = new GameObject(typeof(CoroutineManager).Name);
+                gameObject.hideFlags = HideFlags.HideInHierarchy | HideFlags.HideInInspector | HideFlags.DontSave;
+                Object.DontDestroyOnLoad(gameObject);
+                _monoBehaviour = gameObject.AddComponent<_coroutineBehaviour>();
+            }
+
+            return _monoBehaviour.StartCoroutine(p_enumerator);
+        }
+
+        public static void Kill(Coroutine p_coroutine)
+        {
+            _monoBehaviour.StopCoroutine(p_coroutine);
+        }
+
+        public static void KillAll()
+        {
+            _monoBehaviour.StopAllCoroutines();
+        }
+    }
+}
