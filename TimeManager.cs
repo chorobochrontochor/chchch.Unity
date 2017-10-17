d39697458caf391cf2a1f8bcf1f4d9d7c7a22480
chorobochrontochor <marek.jelinek@live.com>
2017-10-17 21:53:47 +0200

    added time scale tweening

--- TimeManager.cs
+++ TimeManager.cs
@@ -56,6 +56,8 @@ namespace chchch
         public const float NORMAL_TIME = 1f;
         public const float FREEZE_TIME = 0f;
 
+        private static ITween _timeScaleTween;
+
         public static float TimeScale
         {
             get
@@ -64,8 +66,34 @@ namespace chchch
             }
             set
             {
+                stopTimeScaleTween();
+
                 Time.timeScale = value;
             }
+        }
+
+        private static void stopTimeScaleTween()
+        {
+            if (_timeScaleTween != null)
+            {
+                _timeScaleTween.Pause();
+                _timeScaleTween = null;
+            }
+        }
+
+        public static void TweenTimeScale(EasingFunctions.EasingFunction p_easingFunction, double p_from, double p_to, double p_duration)
+        {
+            stopTimeScaleTween();
+
+            Time.timeScale = (float) p_from;
+
+            _timeScaleTween = TweenManager.CreateTween(p_easingFunction, p_duration).OnStep((double p_value) => {
+
+                Time.timeScale = (float) (p_from + (p_to - p_from) * p_value);
+            }).OnFinish((double p_value) => {
+
+                Time.timeScale = (float) p_to;
+            }).Start();
         }
 
         public static ITimeStamp CreateTimeStamp()
