1e2c8112f1fc6e12a64e4a9d5358cfca2a93ab57
chorobochrontochor <marek.jelinek@live.com>
2017-10-17 19:57:48 +0200

    added simple easing functions

---
+++ EasingFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EasingFunctions
+{
+    public delegate double EasingFunction(double p_p);
+
+    public static double Linear(double p_p)
+    {
+        return p_p;
+    }
+
+    public static double EaseInQuad(double p_p)
+    {
+        return p_p * p_p;
+    }
+
+    public static double EaseOutQuad(double p_p)
+    {
+        return p_p * (2 - p_p);
+    }
+
+	public static double EaseInOutQuad(double p_p)
+    {
+        return (p_p < 0.5) ? (2 * p_p * p_p) : ((4 - 2 * p_p) * p_p - 1);
+    }
+}
